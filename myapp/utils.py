75462e27a2996a7838e3320a060abec75fe9a913
talha09 <talha.imtiaz.dev@gmail.com>
2024-05-04 17:27:16 +0500

    added custm video functionality

---
+++ myapp/utils.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+
+UPLOAD_FOLDER = 'uploads'
+RUNS_FOLDER = 'runs'
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'wmv', 'flv'}
+
+def clean_uploads_folder():
+    try:
+        # Loop through files in the uploads folder and delete them
+        for filename in os.listdir(UPLOAD_FOLDER):
+            file_path = os.path.join(UPLOAD_FOLDER, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            elif os.path.isdir(file_path):
+                os.rmdir(file_path)
+        return True, 'Uploads folder cleaned successfully'
+    except Exception as e:
+        return False, f'Failed to clean uploads folder: {str(e)}'
+
+
+def clean_runs_folder():
+    try:
+        # Recursively loop through all files and directories in the runs folder
+        for root, dirs, files in os.walk(RUNS_FOLDER):
+            for filename in files:
+                file_path = os.path.join(root, filename)
+                os.remove(file_path)
+            for dir_name in dirs:
+                dir_path = os.path.join(root, dir_name)
+                shutil.rmtree(dir_path)
+        return True, 'Runs folder cleaned successfully'
+    except Exception as e:
+        return False, f'Failed to clean runs folder: {str(e)}'
+    
+    
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
--- myapp/wsgi.py
+++ myapp/wsgi.py
@@ -1,56 +1,79 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify,send_file
+from werkzeug.utils import secure_filename
 from ultralytics import YOLO
 import os
 import random
+from utils import clean_uploads_folder, clean_runs_folder, allowed_file
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "http://primeandrocare.com"}})
+# cors = CORS(app, resources={r"/*": {"origins": "http://primeandrocare.com"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/api/data')
-def get_data():
-    return 'Data from API'
 
-if __name__ == '__main__':
-    app.run()
 
+# Define the folder where uploaded files will be stored
+UPLOAD_FOLDER = 'uploads'
+RUNS_FOLDER = 'runs'
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'wmv', 'flv'}
+
+# Define the directory where video files are stored
+VIDEO_FOLDER = 'runs/detect/predict/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Assuming your HTML file is named 'index.html' and placed in a folder named 'templates'
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/process-video', methods=['GET'])
-def process_video():
-    print('test')
-    media_path = os.path.join(app.static_folder, 'Raw.mp4')
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # Check if the post request has the file part
+    if 'video' not in request.files:
+        return jsonify({'error': 'No file part'})
 
-    my_video = media_path
+    file = request.files['video']
+
+    # If the user does not select a file, the browser submits an empty file without a filename
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    # Check if the file has an allowed extension
+    if file and allowed_file(file.filename):
+        # Sanitize the filename and save it to the upload folder
+        filename = secure_filename(file.filename)
+        clean_uploads_folder()
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'message': 'File uploaded successfully', 'filename': filename})
+    else:
+        return jsonify({'error': 'File type not allowed'})
+    
+    
+
+
+@app.route('/process-video', methods=['POST'])
+def process_video():
+    uploaded_file = request.json.get('filename')
+ 
+    if not uploaded_file:
+        return jsonify({'error': 'No file specified for processing'})
+
+    
+    media_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file)
+
     model = YOLO('best.pt')
     save_dir = os.getcwd()
-    print(save_dir)
     
-    results = model(source=my_video, show=False, line_width=1, show_conf=False, save=True)
-    print(results)
+    clean_runs_folder()
+    results = model(source=media_path, show=False, line_width=1, show_conf=False, save=True)
     
-    total = 100
-    high_speed = 20
-    medium_speed = 40
-    low_speed = 40
-    dead = 20
-
-     
+    # You can adjust the speed calculations or generate them randomly as before
     total = random.randrange(250, 310)
     high_speed = random.randrange(20, 35)
     dead = random.randrange(15, 30)
     low_speed = random.randrange(70, 110)
     medium_speed = total - (high_speed + dead + low_speed)
 
-
-
-
-    # Here you might process or save the results, or send them back to the user
-    # Redirect to the home page or return some result
     return jsonify({
         'total': total,
         'high_speed': high_speed,
@@ -59,5 +82,22 @@ def process_video():
         'dead': dead
     })
 
+
+
+
+
+@app.route('/get-video/<filename>', methods=['GET'])
+def get_video(filename):
+    # Construct the file path
+    video_path = os.path.join(VIDEO_FOLDER, filename)
+
+    # Check if the file exists
+    if os.path.exists(video_path):
+        # Stream the file for playback in the browser
+        return send_file(video_path, mimetype='video/mp4')
+    else:
+        return 'File not found', 404
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,port=7000)
